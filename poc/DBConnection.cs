8d3e3213219881cbe1e82d03b5acdc3296d0c3ed
LijoVadassery <lijoebox@gmail.com>
2017-06-27 08:51:57 +0530

    first commit

---
+++ poc/DBConnection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace poc
+{
+   public  class DBConnection
+    {
+    public string Sqlconnection(String sql){
+         SqlConnection cnn;
+        string connetionString = null;
+        connetionString = "Data Source=(local);Initial Catalog=HML_PURCHASE;User ID=HML;Password=HML1234";
+        cnn = new SqlConnection(connetionString);     
+        
+           
+    try
+            {
+                                       
+                cnn.Open();
+              //  SqlDataReader myReader = null;
+                SqlCommand myCommand = new SqlCommand(sql, cnn);
+                SqlDataAdapter da = new SqlDataAdapter(myCommand);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                cnn.Close();
+             //   cnn.Close();
+                return ("Database connected");       
+                       }
+            catch (Exception ex)
+            {
+                Console.Write(ex);
+                Console.WriteLine("Database not connected");
+                Console.Read();
+                return("Database not connected");
+            }
+        }
+      
+    }
+}
---
+++ poc/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace poc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            String Sql;
+            DBConnection Dbcon = new DBConnection();
+            Sql = "Select * from FIN_YEAR";
+            Dbcon.Sqlconnection(Sql);
+         }
+    }
+}
